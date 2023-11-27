9c54db09b3fb4d0287c92b7bf5b9567be2c5a5de
slim-chahed <salimshahed33@gmail.com>
2023-11-27 21:47:50 +0000

    first commit

---
+++ CORBAConversion_Server/src/service/ConversionImpl.java
@@ -0,0 +1,13 @@
+package service;
+
+import corbaConversion.IConversionRemotePOA;
+
+// Classe implémentant l'interface IConversionRemotePOA
+public class ConversionImpl extends IConversionRemotePOA {
+
+    // Méthode de conversionMontant, héritée de l'interface IConversionRemote
+    public double conversionMontant(double montant) {
+        // Logique de conversion : multiplie le montant par 3.3 (exemple arbitraire)
+        return montant * 3.3;
+    }
+}
